7a8acd361c83d1a1ace88352d2d4255fd2e5a909
Wijnand Top <w.top@4-dimensions.nl>
2016-09-14 18:12:02 +0200

    Support for thermostat control

---
+++ Fibaro/up.lua
@@ -0,0 +1,45 @@
+
+local hostname = "xxxx"
+local port = "3000"
+
+
+local nefit = net.HTTPClient({timeout=2000})
+
+current = nefit:request("http://"..hostname..":"..port.."/bridge/ecus/rrc/uiStatus", {
+    options={ method = 'GET'},
+    success = function(status)
+        print "callback"
+        data = json.decode(status.data);
+        currentTemp = data.value.TSP;
+        newTemp = tonumber(currentTemp) + 1
+        fibaro:debug("Change temp from "..currentTemp.." to "..newTemp)
+        nefit:request("http://"..hostname..":"..port.."/bridge/heatingCircuits/hc1/temperatureRoomManual", {
+            options={
+                headers = { ["Content-Type"] = "application/json" },
+                data = "{\"value\":"..newTemp.."}",
+                method = 'POST',
+            },
+            success = function(status)
+
+                local result = json.decode(status.data);
+                print("status");
+                fibaro:debug(result);
+
+                if result.status == "ok" then
+                    print("successful");
+                else
+                    print ("failed");
+                    print(status.data);
+                end
+            end,
+            error = function(error)
+                print "ERROR"
+                print(error)
+            end
+        })
+    end
+})
+
+
+
+
